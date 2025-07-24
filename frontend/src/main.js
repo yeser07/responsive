87c538725754ffcfc9193fd5567edbf90ca83705
Yeser Sabillon <yeser07@gmail.com>
2025-07-23 20:25:36 -0600

    modificando index

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// Importa CSS y JS de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js' 
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
 
